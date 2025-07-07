ef49817f570910db296cfece523fc1517803eab1
aabbcc <iyranly@163.com>
2025-07-07 16:27:05 +0800

    fix: fill 0x00 lock witness for MNFT in buildCancelTx

--- src/order/helper.ts
+++ src/order/helper.ts
@@ -59,7 +59,7 @@ export const calculateEmptyCellMinCapacity = (lock: CKBComponents.Script): bigin
 
 export const calculateTransactionFee = (txSize: number): bigint => {
   const ratio = BigNumber(1000)
-  const defaultFeeRate = BigNumber(1100)
+  const defaultFeeRate = BigNumber(2000)
   const fee = BigNumber(txSize).multipliedBy(defaultFeeRate).div(ratio)
   return BigInt(fee.toFixed(0, BigNumber.ROUND_CEIL).toString())
 }
